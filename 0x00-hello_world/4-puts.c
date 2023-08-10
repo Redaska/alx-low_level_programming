927f1c094610d0b45869011f30edce6fb7e4a16d
Essakali <reda.essakali@gmail.com>
2023-08-10 05:56:09 -0700

    Print something

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main use the function puts and not printf
+ * Should end with the value 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
